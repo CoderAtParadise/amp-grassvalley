7f73be006b588aaa4e783dc510af7ca4824d2ee5
Daniel Clarke <dawn0966@gmail.com>
2022-10-23 10:14:45 +1000

    Decode frames from pvs properly as we can now

--- src/decodeUtils.ts
+++ src/decodeUtils.ts
@@ -44,6 +44,30 @@ function getPosition(string: string, subString: string, index: number) {
     return string.split(subString, index).join(subString).length;
 }
 
+const frameMap: { framerate: number; isWholeFrame: boolean }[] = [
+    { framerate: 23.976, isWholeFrame: false },
+    { framerate: 24, isWholeFrame: true },
+    { framerate: 25, isWholeFrame: true },
+    { framerate: 29.97, isWholeFrame: false },
+    { framerate: 30, isWholeFrame: true },
+    { framerate: 48, isWholeFrame: true },
+    { framerate: 50, isWholeFrame: true },
+    { framerate: 59.94, isWholeFrame: false },
+    { framerate: 60, isWholeFrame: true }
+];
+
+function calcFramerate(inframe: number, totalminutes: number) {
+    let framerate: number = 0;
+    for (const frame of frameMap) {
+        const fdr = frame.framerate * 60 * totalminutes;
+        if (inframe >= fdr && inframe < fdr + frame.framerate) {
+            framerate = frame.framerate;
+            if (frame.isWholeFrame) break;
+        }
+    }
+    return framerate;
+}
+
 export function decodeTimecode(timecode: string): string {
     const shortTimecode = (timecode: string) => {
         const frames = timecode.slice(0, 2);
@@ -56,23 +80,31 @@ export function decodeTimecode(timecode: string): string {
         )}:${zeroPad(frames, 2)}`;
     };
 
+    /*
+        PVS when it sends the frames does - framerate * device rate * total minutes + frames 
+        It's stupid so we only handle a max of 60fps which equals roughly a 7 digit frame value based on the math of 23hr * 60min * 60dr * 60fps
+    */
     const longTimecode = (timecode: string) => {
-        const seconds = timecode.slice(4, 6);
-        const minutes = timecode.slice(6, 8);
-        const hours = timecode.slice(8, 10);
+        const str = timecode.split("").reverse().join("").slice(2); //remove checksum
+        const hours = str.slice(0, 2).split("").reverse().join("");
+        const minutes = str.slice(2, 4).split("").reverse().join("");
+        const seconds = str.slice(4, 6).split("").reverse().join("");
+        const fdr = parseInt(str.slice(6).split("").reverse().join(""));
+        const fr = calcFramerate(fdr, parseInt(minutes) + parseInt(hours) * 60); // calculate frame rate
+        const frames = fdr % fr; // mod the frame data sent by pvs by the frame rate
         return `${zeroPad(hours, 2)}:${zeroPad(minutes, 2)}:${zeroPad(
             seconds,
             2
-        )}:00`;
+        )}:${zeroPad(isNaN(frames) ? 0 : frames, 2)}`;
     };
-
-    if (timecode.length <= 10) return shortTimecode(timecode);
-    else if (timecode.length <= 12) return longTimecode(timecode);
-    else {
-        const t = timecode.slice(0, 10);
-        if (getPosition(timecode, t, 2) === 14) return shortTimecode(timecode);
-        else if (getPosition(timecode, t, 2) === 16)
-            return longTimecode(timecode);
-        else return shortTimecode(timecode);
+    //Handle those that follow the spec :)
+    if (timecode.length <= 10 && timecode.length >= 8)
+        return shortTimecode(timecode);
+    //Handle pvs doing stupid things
+    else if (timecode.length >= 12 && timecode.length <= 17) {
+        return longTimecode(timecode);
+    } else {
+        // Return an invalid timecode as decoding becomes impossible
+        return "--:--:--:--";
     }
 }
